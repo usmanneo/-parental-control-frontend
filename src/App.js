128fcd8e86f31e20eb5412f2ba49892e372ec8c0
usmanneo <h4k3rtools@gmail.com>
2024-10-13 09:31:20 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ControlPanel from './components/ControlPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <ControlPanel />
+    </div>
+  );
+}
+
+export default App;
