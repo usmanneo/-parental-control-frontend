380dc7cc140134dc7865c35eefafd85356dbfe3e
usmanneo <h4k3rtools@gmail.com>
2024-10-14 04:54:51 -0700

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the Parental Control Panel</h1>
+    <ControlPanel />
+  </div>
+</template>
+
+<script>
+import ControlPanel from '../components/ControlPanel.vue';
+
+export default {
+  components: {
+    ControlPanel
+  }
+}
+</script>
